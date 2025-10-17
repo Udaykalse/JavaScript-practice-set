83554e91d0b3776be1412947d178ecfe19e8c709
Udaykalse <udaykalse123@gmail.com>
2025-10-17 23:27:57 +0530

    last 5 ex of Oct 17

--- oct14/my-vite-app/src/components/F-day1/app11.jsx
+++ oct14/my-vite-app/src/components/F-day1/app11.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function app11() {
+    const [task, setTask] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const addTask = () => {
+    if (task.trim()) {
+      setTodos([...todos, task]);
+      setTask("");
+    }
+  };
+
+  const deleteTask = (index) => {
+    setTodos(todos.filter((_, i) => i !== index));
+  };
   return (
     <div>
-      11
+         <div> <h2>Todo-list</h2>
+      <input value={task} onChange={(e) => setTask(e.target.value)} />
+      <button onClick={addTask}>Add</button>
+      <ul>
+        {todos.map((t, i) => (
+          <li key={i}>
+            {t} <button onClick={() => deleteTask(i)}>❌</button>
+          </li>
+        ))}
+      </ul>
+    </div>
     </div>
   )
 }
--- oct14/my-vite-app/src/components/F-day1/app15.jsx
+++ oct14/my-vite-app/src/components/F-day1/app15.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function app15() {
+  const [task, setTask] = useState('');
+  const [todos, setTodos] = useState([]);
+
+
+  const addTask = () => {
+    if (task.trim()) {
+      setTodos([...todos, task]);
+      setTask("");
+    }
+  };
+  const deleteTask = (idX) => {
+    setTodos(todos.filter((_, i) => i !== idX))
+  };
+
   return (
     <div>
-      16
+      <h2>Todo-List 6</h2>
+      <input type="text" value={task} onChange={(e) => setTask(e.target.value)} />
+      <button onClick={addTask}>Add</button>
+
+      <ul>
+        {
+          todos.map((t, i) => (
+            <li key={i}>{t} <button onClick={() => deleteTask(i)}>X</button></li>
+          ))
+        }
+      </ul>
+
     </div>
   )
 }
